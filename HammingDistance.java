5eeaec89110f27d4b966d3aed9d23be2e22cb66f
Hakan Bagci <hbagci@gmail.com>
2017-03-04 16:10:01 +0200

    Update HammingDistance.java

--- HammingDistance.java
+++ HammingDistance.java
@@ -2,8 +2,8 @@ public int hammingDistance(int x, int y) {
     int result = x ^ y;
     int count = 0;
 
-    for(int i = 0; i < 32; i++) {
-        if((result & 1) == 1) {
+    for (int i = 0; i < 32; i++) {
+        if ((result & 1) == 1) {
             count++;
         }
 
